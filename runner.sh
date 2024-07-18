ea3aa5b4e9931c6839723fcbc4e7f04990ad0fa6
Leonardo Barberi <lbarberi@student.ethz.ch>
2024-07-18 10:30:56 +0200

    training notebook creation

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=10:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=icsnode05
+#SBATCH --nodelist=icsnode06
 
 # # Send some noteworthy information to the output log
 echo "Running on node: $(hostname)"
